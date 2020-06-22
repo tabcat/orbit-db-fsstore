7856a6f5783217dcfc9db0fa2a76eefad97de48c
anders <andersb@tuta.io>
2020-06-22 00:44:57 -0500

    FSStore validates path, dest, name

--- src/FSStore.js
+++ src/FSStore.js
@@ -3,9 +3,11 @@
 
 const Store = require('orbit-db-store')
 const FSIndex = require('./FSIndex')
-const { joinPath, exists, content, read, tree, ls, opcodes } = require('./FS')
+const { joinPath, pathValid, nameValid, opcodes, ...fs } = require('./FS')
 
 const errors = {
+  pathValidNo: (path) => new Error(`path ${path} is not valid`),
+  nameValidNo: (name) => new Error(`name ${name} is not valid`),
   pathExistNo: (path) => new Error(`path '${path}' does not exist`),
   pathExistYes: (path) => new Error(`path '${path}' already exists`)
 }
@@ -19,27 +21,33 @@ class FSStore extends Store {
     this._type = FSStore.type
 
     this.joinPath = joinPath
-    this.exists = (path = '') => exists(this._index.get(), path)
-    this.content = (path = '') => content(this._index.get(), path)
-    this.read = (path = '') => read(this._index.get(), path)
-    this.tree = (path = '') => tree(this._index.get(), path)
-    this.ls = (path = '') => ls(this._index.get(), path)
+    this.exists = (path = '') => fs.exists(this._index.get(), path)
+    this.content = (path = '') => fs.content(this._index.get(), path)
+    this.read = (path = '') => fs.read(this._index.get(), path)
+    this.tree = (path = '') => fs.tree(this._index.get(), path)
+    this.ls = (path = '') => fs.ls(this._index.get(), path)
   }
 
   static get type () { return type }
 
   mkdir (path, name) {
+    if (!pathValid(path)) throw errors.pathValidNo(path)
+    if (!nameValid(name)) throw errors.nameValidNo(name)
     if (!this.exists(path)) throw errors.pathExistNo(path)
     if (this.exists(joinPath(path, name))) throw errors.pathExistYes(joinPath(path, name))
     return this._addOperation({ op: opcodes.MKDIR, path, name })
   }
 
   rmdir (path) {
+    if (!pathValid(path)) throw errors.pathValidNo(path)
     if (!this.exists(path)) throw errors.pathExistNo(path)
     return this._addOperation({ op: opcodes.RMDIR, path })
   }
 
   mvdir (path, dest, name) {
+    if (!pathValid(path)) throw errors.pathValidNo(path)
+    if (!pathValid(dest)) throw errors.pathValidNo(dest)
+    if (!nameValid(name)) throw errors.nameValidNo(name)
     if (!this.exists(path)) throw errors.pathExistNo(path)
     if (!this.exists(dest)) throw errors.pathExistNo(dest)
     if (this.exists(joinPath(dest, name))) throw errors.pathExistYes(joinPath(dest, name))
@@ -47,6 +55,9 @@ class FSStore extends Store {
   }
 
   cpdir (path, dest, name) {
+    if (!pathValid(path)) throw errors.pathValidNo(path)
+    if (!pathValid(dest)) throw errors.pathValidNo(dest)
+    if (!nameValid(name)) throw errors.nameValidNo(name)
     if (!this.exists(path)) throw errors.pathExistNo(path)
     if (!this.exists(dest)) throw errors.pathExistNo(dest)
     if (this.exists(joinPath(dest, name))) throw errors.pathExistYes(joinPath(dest, name))
@@ -54,6 +65,8 @@ class FSStore extends Store {
   }
 
   mk (path, name) {
+    if (!pathValid(path)) throw errors.pathValidNo(path)
+    if (!nameValid(name)) throw errors.nameValidNo(name)
     if (!this.exists(path)) throw errors.pathExistNo(path)
     if (this.exists(joinPath(path, name))) throw errors.pathExistYes(joinPath(path, name))
     return this._addOperation({ op: opcodes.MK, path, name })
@@ -65,11 +78,15 @@ class FSStore extends Store {
   }
 
   rm (path) {
+    if (!pathValid(path)) throw errors.pathValidNo(path)
     if (!this.exists(path)) throw errors.pathExistNo(path)
     return this._addOperation({ op: opcodes.RM, path })
   }
 
   mv (path, dest, name) {
+    if (!pathValid(path)) throw errors.pathValidNo(path)
+    if (!pathValid(dest)) throw errors.pathValidNo(dest)
+    if (!nameValid(name)) throw errors.nameValidNo(name)
     if (!this.exists(path)) throw errors.pathExistNo(path)
     if (!this.exists(dest)) throw errors.pathExistNo(dest)
     if (this.exists(joinPath(dest, name))) throw errors.pathExistYes(joinPath(dest, name))
@@ -77,6 +94,9 @@ class FSStore extends Store {
   }
 
   cp (path, dest, name) {
+    if (!pathValid(path)) throw errors.pathValidNo(path)
+    if (!pathValid(dest)) throw errors.pathValidNo(dest)
+    if (!nameValid(name)) throw errors.nameValidNo(name)
     if (!this.exists(path)) throw errors.pathExistNo(path)
     if (!this.exists(dest)) throw errors.pathExistNo(dest)
     if (this.exists(joinPath(dest, name))) throw errors.pathExistYes(joinPath(dest, name))
